9729831ee24e95f3d3d061f426bf96ff512bb83a
Nikolay Khlyupikov <xoppopp13@gmail.com>
2023-11-06 12:30:07 +0300

    polished

--- src/test/java/ru/netology/javaqa/javaqamvn/services/VocationCalcTest.java
+++ src/test/java/ru/netology/javaqa/javaqamvn/services/VocationCalcTest.java
@@ -1,10 +1,8 @@
 package ru.netology.javaqa.javaqamvn.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
 public class VocationCalcTest {
 
