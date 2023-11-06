fcb7732c2b7b18249cb7b09d464658e403dcf576
Nikolay Khlyupikov <xoppopp13@gmail.com>
2023-11-06 12:26:23 +0300

    done

---
+++ src/test/java/ru/netology/javaqa/javaqamvn/services/VocationCalcTest.java
@@ -0,0 +1,20 @@
+package ru.netology.javaqa.javaqamvn.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class VocationCalcTest {
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/vocation.csv")
+    void amountOfMonths(int expected, int income, int expenses, int threshold) {
+        VocationCalc service = new VocationCalc();
+
+
+        int actual = service.calculate(income, expenses, threshold);
+        Assertions.assertEquals(expected, actual);
+    }
+}
